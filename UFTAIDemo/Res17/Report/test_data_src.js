e17cea4fa1c50f75b6f9247e4a6767d313bc3bc1
usmanaligit <usman.ali@microfocus.com>
2023-01-13 12:43:56 +0800

    uftt

---
+++ UFTAIDemo/Res17/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\abdulrus\\OneDrive - Micro Focus\\Documents\\Unified Functional Testing\\UFT AI Demo\\Res17',namePairs:[]}]};
